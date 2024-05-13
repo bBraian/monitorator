5bb4a41e44c325397f57fcea781455536fe51d6a
bBraian <braianvoficial@gmail.com>
2024-05-13 13:21:37 -0300

    feat: implementing dashboard screen

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -5,16 +5,27 @@ export function Dashboard() {
     <>
       <Helmet title="Dashboard" />
       <div className="flex flex-col gap-4">
-        <div className='w-full flex justify-center'>
-            <h1 className="text-3xl font-semibold tracking-tight">Todos os serviços estão <span className="text-green-500">online</span></h1>
+        <div className='w-full flex items-center flex-col gap-4'>
+            <h1 className="text-3xl font-semibold tracking-tight">Todos os serviços <span className="text-green-500">online</span></h1>
+            <div className='bg-zinc-100 dark:bg-zinc-800 py-3 px-4 rounded-full'>
+                <span className='text-sm tracking-tight'>Última atualização: <span className='font-semibold'>18 Out</span> as <span className='font-semibold'>17:36</span></span>
+            </div>
         </div>
 
-        <div className="grid grid-cols-4 gap-4">    
-          adadas
-        </div>
-
-        <div className="grid grid-cols-9 gap-4">
-            oi
+        <div className="flex flex-col w-full pt-6">
+            <div className="flex w-full justify-between pb-5">
+                <h2>Services</h2>
+                <div className="flex gap-2">
+                    <div>Day</div>
+                    <div>Week</div>
+                    <div>Month</div>
+                </div>
+            </div>
+            <div className='bg-zinc-100 dark:bg-zinc-800 p-8 rounded-2xl'>
+                <div>asd</div>
+                <div>asd</div>
+                <div>asd</div>
+            </div>
         </div>
       </div>
     </>
