da11ca837a8e6aceabb1c5139a3f4c6b064a563d
bBraian <braianvoficial@gmail.com>
2024-05-16 20:25:47 -0300

    styling: dashboard

--- src/pages/app/dashboard/dashboard.tsx
+++ src/pages/app/dashboard/dashboard.tsx
@@ -2,6 +2,7 @@ import { getServiceStatus } from '@/@fakeDB/servicesStatus'
 import { Helmet } from 'react-helmet-async'
 import { CircleCheck } from 'lucide-react'
 import { useState } from 'react'
+import { Separator } from '@radix-ui/react-separator'
 
 const servicesStatus = getServiceStatus()
 console.log(servicesStatus)
@@ -17,6 +18,14 @@ const timeIntervals = [
   {id: "week", name: "Semana" },
   {id: "month", name: "Mês" },
 ]
+
+const services = [
+  {id: 1, name: 'Service 1', dailyStatus: 98.7},
+  {id: 2, name: 'Service 2', dailyStatus: 45},
+  {id: 3, name: 'Service 1', dailyStatus: 100},
+]
+
+
 
 export function Dashboard() {
   const [intervalTimeShowing, setIntervalTimeShowing] = useState("day")
@@ -36,76 +45,44 @@ export function Dashboard() {
                 <h2 className='text-muted-foreground'>Serviços</h2>
                 <div className="flex gap-2">
                   {timeIntervals.map(filter => (
-                    <div data-active={intervalTimeShowing == filter.id} onClick={() => setIntervalTimeShowing(filter.id)} className='text-muted-foreground hover:text-foreground data-[active=true]:text-foreground cursor-pointer'>{filter.name}</div>
+                    <div key={filter.id} data-active={intervalTimeShowing == filter.id} onClick={() => setIntervalTimeShowing(filter.id)} className='text-muted-foreground hover:text-foreground data-[active=true]:text-foreground cursor-pointer'>{filter.name}</div>
                   ))}
                 </div>
             </div>
-            <div className='bg-zinc-100 dark:bg-zinc-800 p-8 rounded-2xl'>
-
-              <div className='first:pt-0 last:pb-0 py-10 last:border-b-0 border-b-2 dark:border-muted-foreground border-gray-300'>
-                <div className='flex justify-between pb-2'>
-                  <div className='flex gap-2'>
-                    <CircleCheck color="#2fc700"/> 
-                    <h2 className='font-semibold'>Service1</h2>
+            <div className='bg-zinc-100 dark:bg-zinc-800 p-5 rounded-2xl'>
+              {services.map((service, index) => {
+                const isLastIndex = services.length === index + 1
+                return (
+                  <div className='first:pt-0 last:pb-0' key={service.id}>
+                  <div>
+                    <div className='cursor-pointer hover:bg-zinc-900 rounded-lg py-3 px-5'>
+                      <div className='flex justify-between pb-2'>
+                        <div className='flex gap-2'>
+                          <CircleCheck color="#2fc700"/> 
+                          <h2 className='font-semibold'>{service.name}</h2>
+                        </div>
+                        <div className='text-lime-600 font-semibold'>{service.dailyStatus}%</div>
+                      </div>
+                      <ul className='flex w-full dark:bg-zinc-800 border-2 dark:border-zinc-600 border-white bg-white h-14 p-2 rounded-xl'>
+                        {servicesStatus.map((stats) => (
+                          <li 
+                            key={stats.id} 
+                            className={`flex ${colorsStatus[stats.status]} border-x-2 dark:border-x-zinc-800 border-x-white h-full w-full first:rounded-l-lg first:border-l-0 last:rounded-r-lg last:border-r-0`}
+                          />
+                        ))}
+                      </ul>
+                      <div className='flex justify-between pt-2'>
+                        <div className='text-sm text-muted-foreground tracking-tigh'>Hà 60 dias </div>
+                        <div className='text-sm text-muted-foreground tracking-tigh'>Hoje</div>
+                      </div>
+                    </div>
                   </div>
-                  <div className='text-lime-600 font-semibold'>99.99%</div>
+                  {!isLastIndex && (
+                    <Separator orientation="vertical" className="h-0.5 dark:bg-muted-foreground bg-gray-300 my-5 mx-5" />
+                  )}
                 </div>
-                <ul className='flex w-full dark:bg-zinc-800 border-2 dark:border-zinc-600 border-white bg-white h-16 p-2 rounded-xl'>
-                  {servicesStatus.map((stats) => (
-                    <li 
-                      key={stats.id} 
-                      className={`flex ${colorsStatus[stats.status]} border-x-2 dark:border-x-zinc-800 border-x-white h-full w-full first:rounded-l-lg first:border-l-0 last:rounded-r-lg last:border-r-0`}
-                    />
-                  ))}
-                </ul>
-                <div className='flex justify-between pt-2'>
-                  <div className='text-sm text-muted-foreground tracking-tigh'>Hà 60 dias </div>
-                  <div className='text-sm text-muted-foreground tracking-tigh'>Hoje</div>
-                </div>
-              </div>
-
-              <div className='first:pt-0 last:pb-0 py-10 last:border-b-0 border-b-2 dark:border-muted-foreground border-gray-300'>
-                <div className='flex justify-between pb-2'>
-                  <div className='flex gap-2'>
-                    <CircleCheck color="#2fc700"/> 
-                    <h2 className='font-semibold'>Service1</h2>
-                  </div>
-                  <div className='text-lime-600 font-semibold'>99.99%</div>
-                </div>
-                <ul className='flex w-full dark:bg-zinc-800 border-2 dark:border-zinc-600 border-white bg-white h-16 p-2 rounded-xl'>
-                  {servicesStatus.map((stats) => (
-                    <li 
-                      key={stats.id} 
-                      className={`flex ${colorsStatus[stats.status]} border-x-2 dark:border-x-zinc-800 border-x-white h-full w-full first:rounded-l-lg first:border-l-0 last:rounded-r-lg last:border-r-0`}
-                    />
-                  ))}
-                </ul>
-                <div className='flex justify-between pt-2'>
-                  <div className='text-sm text-muted-foreground'>Hà 60 dias </div>
-                  <div className='text-sm text-muted-foreground'>Hoje</div>
-                </div>
-              </div>
-              <div className='first:pt-0 last:pb-0 py-10 last:border-b-0 border-b-2 dark:border-muted-foreground border-gray-300'>
-                <div className='flex justify-between pb-2'>
-                  <div className='flex gap-2'>
-                    <CircleCheck color="#2fc700"/> 
-                    <h2 className='font-semibold'>Service1</h2>
-                  </div>
-                  <div className='text-lime-600 font-semibold'>99.99%</div>
-                </div>
-                <ul className='flex w-full dark:bg-zinc-800 border-2 dark:border-zinc-600 border-white bg-white h-16 p-2 rounded-xl'>
-                  {servicesStatus.map((stats) => (
-                    <li 
-                      key={stats.id} 
-                      className={`flex ${colorsStatus[stats.status]} border-x-2 dark:border-x-zinc-800 border-x-white h-full w-full first:rounded-l-lg first:border-l-0 last:rounded-r-lg last:border-r-0`}
-                    />
-                  ))}
-                </ul>
-                <div className='flex justify-between pt-2'>
-                  <div className='text-sm text-muted-foreground'>Hà 60 dias </div>
-                  <div className='text-sm text-muted-foreground'>Hoje</div>
-                </div>
-              </div>
+                )
+              })}
             </div>
         </div>
       </div>
